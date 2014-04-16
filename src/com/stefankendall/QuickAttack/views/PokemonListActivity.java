c621eea53098d535ba476f4848d409b2c0809950
Stefan Kendall <stefankendall@gmail.com>
2014-04-16 19:54:04 -0400

    Added crashlytics

--- src/com/stefankendall/QuickAttack/views/PokemonListActivity.java
+++ src/com/stefankendall/QuickAttack/views/PokemonListActivity.java
@@ -7,11 +7,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import com.crashlytics.android.Crashlytics;
 import com.stefankendall.QuickAttack.SingleFragmentActivity;
 import com.stefankendall.QuickAttack.data.DataLoader;
 import com.stefankendall.QuickAttack.views.PokemonListFragment;
 
 public class PokemonListActivity extends SingleFragmentActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Crashlytics.start(this);
+    }
+
     @Override
     protected Fragment createFragment() {
         return new PokemonListFragment();
