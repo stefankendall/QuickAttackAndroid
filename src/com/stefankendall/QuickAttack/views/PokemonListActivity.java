2f0cbc2ecef9e7decdd0a059da89997cdf9a5d9f
Stefan Kendall <stefankendall@gmail.com>
2014-04-29 20:16:04 -0400

    Added load hooks for my pokemon

--- src/com/stefankendall/QuickAttack/views/PokemonListActivity.java
+++ src/com/stefankendall/QuickAttack/views/PokemonListActivity.java
@@ -5,12 +5,14 @@ import android.os.Bundle;
 import com.crashlytics.android.Crashlytics;
 import com.stefankendall.QuickAttack.R;
 import com.stefankendall.QuickAttack.SingleFragmentActivity;
+import com.stefankendall.QuickAttack.data.MyPokemonStore;
 
 public class PokemonListActivity extends SingleFragmentActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Crashlytics.start(this);
+        MyPokemonStore.instance().load();
         setupNavigation();
     }
 
