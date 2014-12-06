424c5ffe89dd4ea4ee83c59cf4675e64f91ff2ca
Stefan Kendall <stefankendall@gmail.com>
2014-12-06 11:32:31 -0500

    Commit heinous crimes and sacrified a goat to get google play services to work in an intellij project

--- src/com/stefankendall/QuickAttack/SingleFragmentActivity.java
+++ src/com/stefankendall/QuickAttack/SingleFragmentActivity.java
@@ -5,7 +5,6 @@ import android.app.Fragment;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
